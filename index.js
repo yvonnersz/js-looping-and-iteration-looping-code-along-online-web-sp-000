8001c591e11ae000b36c49f413ed69f33a24e608
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-28 16:35:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  let thankYouNotes = [];
+  for (let i=0; i<names.length; i++) {
+    thankYouNotes.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouNotes;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(integer) {
+  while (integer>=0) {
+    console.log(integer);
+    integer--;
+  }
+}
+
+countDown(10);
