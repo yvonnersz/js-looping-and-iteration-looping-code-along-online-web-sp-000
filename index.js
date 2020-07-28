b2e2457163b6e190b2ff2520b19b7b08b8ac873e
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-28 15:40:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function writeCards(names, event) {
   for (let i=0; i<names.length, i++) {
-    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
 }
 
