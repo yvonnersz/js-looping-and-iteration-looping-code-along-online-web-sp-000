f029d7dc360957bf7b7f4927cbb0adfead807567
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-28 15:37:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const people = ["Lisa", "Kaitlin", "Jan"];
 const event = "birthday";
 
 function writeCards(people, event) {
-  array = []
+  let array = []
   for (let i=0; i<people.length; i++) {
     array.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
   }
